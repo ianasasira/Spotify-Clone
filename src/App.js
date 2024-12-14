3438989ba6627deda397a107a4808305a1e41a67
ian asasira <you@example.com>
2024-12-14 16:41:35 +0300

    Redirect to Music Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import './App.css';
 import Login from './Login.js';
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 import { getTokenFromUrl } from './spotify.js';
 
 //ae22bd10c36a4a14b7022bc6111fd293 spotify id
 function App() {
+
+  // state hooks for variables
+  //temporary location for the token when it refereshs it becomes null
+  const[token, setToken]= useState(null);
+
+
   //run code based on token
 
+  //the convention is we use _ for the one we are to use
   useEffect(()=>{
-     const token = getTokenFromUrl();
-     console.log("The Token is",token);
-     
+     const hash = getTokenFromUrl();
+     window.location.hash="";
+     const _token = hash.access_token;
+
+     if(_token){
+      //hence the state variable token becomes _token
+      setToken(_token)
+     }
   },[]);
   
   return (
     <div className="App">
-
-    <Login/>
+    {
+      //jsx
+      token ? (
+        <h1>MUSIC PAGE</h1>
+      ):(
+        <Login/>
+      )
+    }
+    
 
     </div>
   );
