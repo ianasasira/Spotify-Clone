287c2a22324138eeb98000ce6da4ccb3701112fc
ian asasira <you@example.com>
2024-12-14 15:05:09 +0300

    creating Login Page

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Login.css'
+
+function Login() {
+  return (
+    <div className='login'>
+
+    <h2>LOGIN</h2>
+
+    <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png" alt="" />
+     <button> LOGIN INTO SPOTIFY ACCOUNT</button>
+
+    </div>
+
+  )
+}
+
+export default Login
